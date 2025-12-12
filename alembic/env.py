b27caf73781d1b0b2aa40aaa8df262130e89f34f
DevendraKumar <devendragangwar097@gmail.com>
2025-12-13 02:36:18 +0530

    changes updated

--- alembic/env.py
+++ alembic/env.py
@@ -19,7 +19,7 @@ SQLALCHAMY_DATABASE_URL = (
     f"@{settings.MYSQLHOST}:{settings.MYSQLPORT}/{settings.MYSQLDATABASE}"
 )
 
-config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
+config.set_main_option("sqlalchemy.url", SQLALCHAMY_DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
